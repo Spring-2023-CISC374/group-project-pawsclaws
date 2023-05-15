eaad6d689f2f1f1a683ea848b6230022bf69bb5e
bryan <bdelled@udel.edu>
2023-05-15 13:23:37 -0400

    Commented out PageScene start

--- src/scenes/startScene.ts
+++ src/scenes/startScene.ts
@@ -15,7 +15,7 @@ export default class startScene extends Phaser.Scene {
         startButton.setInteractive()
         startButton.on('pointerdown', () => {
             this.scene.start("helloworldscene")
-            this.scene.start("PageScene")
+            // this.scene.start("PageScene")
             this.scene.launch("InstructionsScene")
         })
     }
